66b2575cbd2ee3f8fc1aa39b31ae0eeb5ec656ec
tzhao <tzhao@users.noreply.github.com>
2022-02-26 10:47:01 -0500

    Update test.py

--- TonganZhao/PaulShinn/python test/test.py
+++ TonganZhao/PaulShinn/python test/test.py
@@ -1,16 +1,20 @@
 # Python program to illustrate
 # function with main
+
+import pandas as pd
+import csv
+import re
+
 def getFileName():
-	result = (input("Enter filename: "))
-	return result
+	input_file = (input("Enter filename: "))
+	return input_file
 
 #def PrintStep():
 #    for step in range(5):    
 #        print(step)
 
 def readCSVFile(FileName):
-	import csv
-
+	
 	with open(FileName) as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		line_count=0
@@ -23,10 +27,25 @@ def readCSVFile(FileName):
 				line_count+=1
 		print(f'Processed {line_count} lines')
 
-def readCSVFile2(FileName):
-	import pandas
-	df=pandas.read_csv(FileName)
-	print(df)
+#read the CSV using Pandas
+def readCSVFile2(FileName, outfile):
+	df=pd.read_csv(FileName)
+
+	print (len(df))
+	f=open(outfile, "w")
+
+	#reorganizes the columns in the CSV and
+	#extracts just the number from the concentration column
+	#before writing them to a file
+	for i, row in df.iterrows():
+		Conc=re.findall("\d+", row['Concentration'])[0]
+		#row_to_write= (i, row['Plate'], row['Well'], row['Sample ID'], Conc, row['Barcode'])
+		row_to_write= str(i) + "," + str(row['Plate']) + "," + str(row['Well']) + "," + str(row['Sample ID']) + "," + str(Conc) + "," + str(row['Barcode']) + "\n"
+		#print(row_to_write)
+		f.write(row_to_write)
+
+
+	f.close()
 
 def Main():
 	print("Started")
@@ -39,11 +58,16 @@ def Main():
 	#f = open(FileName, "rt")	
 	#print(f.read())
 
+	#extracts the root file name and appends a 2 onto it
+	outfile=re.findall("(\S+).csv", FileName)[0] + "2.csv"
+	
+	print(outfile)
+
 	#read a CSV through regular file reader
-	readCSVFile(FileName)
+	#readCSVFile(FileName)
 
 	#read a CSV using pandas
-	readCSVFile2(FileName)
+	readCSVFile2(FileName, outfile)
 
 # now we are required to tell Python
 # for 'Main' function existence
